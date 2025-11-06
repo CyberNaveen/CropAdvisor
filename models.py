654557855bd43b1f0b6c997a7a008d2500b7c122
CyberNaveen <naveenkumar20061704@gmail.com>
2025-11-06 20:53:48 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,8 +6,7 @@ db = SQLAlchemy()
 class UserRecord(db.Model):
     __tablename__ = "userRecords"
 
-    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(120), primary_key=True nullable=False)
     username = db.Column(db.String(50), nullable=False, unique=True, index=True)
     email = db.Column(db.String(255), nullable=False, unique=True, index=True)
     mobileNumber = db.Column(db.String(20), nullable=True)
