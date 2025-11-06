7caa51e46b14fa79e11a2feb8766c5b61d7204f5
CyberNaveen <naveenkumar20061704@gmail.com>
2025-11-06 21:04:53 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ db = SQLAlchemy()
 class UserRecord(db.Model):
     __tablename__ = "userRecords"
 
-    name = db.Column(db.String(120), primary_key=True nullable=False)
-    username = db.Column(db.String(50), nullable=False, unique=True, index=True)
+    name = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(50), primary_key=True, nullable=False, unique=True, index=True)
     email = db.Column(db.String(255), nullable=False, unique=True, index=True)
     mobileNumber = db.Column(db.String(20), nullable=True)
     password = db.Column(db.String(255), nullable=False)  # hashed password
