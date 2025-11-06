420fc68dfdd1f0f6ea5ca48023f5f9f816a67be8
CyberNaveen <naveenkumar20061704@gmail.com>
2025-11-06 21:26:04 +0530

    Update models.py

--- models.py
+++ models.py
@@ -5,11 +5,11 @@ db = SQLAlchemy()
 
 class UserRecord(db.Model):
     __tablename__ = "userRecords"
-
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(120), nullable=False)
-    username = db.Column(db.String(50), primary_key=True, nullable=False, unique=True, index=True)
+    username = db.Column(db.String(50), nullable=False, unique=True, index=True)
     email = db.Column(db.String(255), nullable=False, unique=True, index=True)
     mobileNumber = db.Column(db.String(20), nullable=True)
-    password = db.Column(db.String(255), nullable=False)  # hashed password
+    password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
