1bc04719b253d481cf2c9ae106e7c1398a60fb8a
CyberNaveen <naveenkumar20061704@gmail.com>
2025-11-07 15:40:56 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
 db = SQLAlchemy()
 
 class UserRecord(db.Model):
     __tablename__ = "userRecords"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), primary_key=True)
     name = db.Column(db.String(120), nullable=False)
-    username = db.Column(db.String(50), nullable=False, unique=True, index=True)
-    email = db.Column(db.String(255), nullable=False, unique=True, index=True)
-    mobileNumber = db.Column(db.String(20), nullable=True)
+    email = db.Column(db.String(255), unique=True, index=True, nullable=False)
+    mobileNumber = db.Column(db.String(20))
     password = db.Column(db.String(255), nullable=False)
